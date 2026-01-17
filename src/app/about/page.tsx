20c281112eaa5c0f0b93252e5343d36389436d88
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-17 23:41:32 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -53,7 +53,7 @@ function page() {
 
         {/* navbar */}
         <div
-          className={`w-[95%] lg:w-[90%] z-20 fixed top-5 h-auto backdrop-blur-2xl ${dark ? "bg-black/15" : "bg-white/15"} duration-200 ease-in-out px-5 flex justify-between items-center py-5 lg:py-2 rounded-full border ${dark ? "border-gray-400" : "border-zinc-600"}`}>
+          className={`w-[95%] lg:w-[90%] z-20 fixed top-5 h-auto backdrop-blur-2xl ${dark ? "bg-black/15" : "bg-white/15"} duration-200 ease-in-out px-5 flex justify-between items-center py-3 lg:py-2 rounded-full border ${dark ? "border-gray-400" : "border-zinc-600"}`}>
           <Link href='/' className={`${dark ? "text-white" : "text-black"} duration-200 ease-in-out cursor-pointer font-semibold text-xl font-Nearo`}>Pixel Labs</Link>
 
           <div className={`w-auto lg:hidden flex justify-center items-center gap-5`}>
@@ -108,3 +108,4 @@ function page() {
 }
 
 export default page
+
